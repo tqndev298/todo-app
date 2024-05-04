acdfc02dc0fcce5660bd5e17ef92d80ac9193b43
I am TQN <uznaruto298@gmail.com>
2024-05-04 18:33:11 +0700

    commit firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/widgets/task_widget.dart';
 
 import 'auth/main_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: FirebaseOptions(
-          apiKey: 'AIzaSyDwdyEv7vXhUeP8gHbxVWKXZtDgElDUcF4',
-          appId: '1:506821556581:android:ed6eea6e2d4dfd5eaf498c',
-          messagingSenderId: 'messagingSenderId',
-          projectId: 'to-do-list-app-77995'));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
